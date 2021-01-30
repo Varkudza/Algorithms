dcb1421e686b1a17b53bbeaa631a2784a05f57d4
Aleksey Samoylov <asamoylov@kpr-it.ru>
2021-01-30 13:44:24 +0300

    Бинарный поиск

---
+++ Algorithms/Searching.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+    public static class Searching
+    {
+        /// <summary>
+        /// метод для рекурсивного бинарного поиска
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="searchedValue"></param>
+        /// <param name="first"></param>
+        /// <param name="last"></param>
+        /// <returns></returns>
+        public static int BinarySearch(int[] array, int searchedValue, int first, int last)
+        {
+            //границы сошлись
+            if (first > last)
+            {
+                //элемент не найден
+                return -1;
+            }
+
+            //средний индекс подмассива
+            var middle = (first + last) / 2;
+            //значение в средине подмассива
+            var middleValue = array[middle];
+
+            if (middleValue == searchedValue)
+            {
+                return middle;
+            }
+            else
+            {
+                if (middleValue > searchedValue)
+                {
+                    //рекурсивный вызов поиска для левого подмассива
+                    return BinarySearch(array, searchedValue, first, middle - 1);
+                }
+                else
+                {
+                    //рекурсивный вызов поиска для правого подмассива
+                    return BinarySearch(array, searchedValue, middle + 1, last);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="array"></param>
+        /// <param name="searchedValue"></param>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns></returns>
+        public static int BinarySearchIterative(int[] array, int searchedValue, int left, int right)
+        {
+            //пока не сошлись границы массива
+            while (left <= right)
+            {
+                //индекс среднего элемента
+                var middle = (left + right) / 2;
+
+                if (searchedValue == array[middle])
+                {
+                    return middle;
+                }
+                else if (searchedValue < array[middle])
+                {
+                    //сужаем рабочую зону массива с правой стороны
+                    right = middle - 1;
+                }
+                else
+                {
+                    //сужаем рабочую зону массива с левой стороны
+                    left = middle + 1;
+                }
+            }
+            //ничего не нашли
+            return -1;
+        }
+    }
+}
